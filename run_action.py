0e99cb1993f1827f8e91462ff819537301361cf0
itisreal9527 <82878931+itisreal9527@users.noreply.github.com>
2022-03-16 20:57:33 +0800

    Update run_action.py

--- run_action.py
+++ run_action.py
@@ -2,8 +2,8 @@ from login_new import login_new
 from clock_in import clock_in
 import os
 
-stu_id = str(os.env['STUID'])
-pwd = str(os.env['STUPWD'])
+stu_id = str(os.environ['STUID'])
+pwd = str(os.environ['STUPWD'])
 
 login_new(stu_id, pwd)
 clock_in(stu_id)
